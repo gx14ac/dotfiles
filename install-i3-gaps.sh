71a03d3b143cc3f66dfa592fc0a52634d1213313
flekystyley <flekystyley@gmail.com>
2019-11-10 01:19:31 +0900

    create 'install-i3-gaps-.sh;'

---
+++ install-i3-gaps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install git libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y && git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps && autoreconf --force --install && rm -rf build/ && mkdir -p build && cd build/ && ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers && make && sudo make install
+
+
